19d40338af0f6c241d2cb95e1e13ed5ee14e6043
wangxinyu1008 <2401816735@qq.com>
2020-09-15 17:21:01 +0800

    测试commit

---
+++ demo01/src/main/java/com/kgc/App.java
@@ -0,0 +1,14 @@
+package com.kgc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("测试commit");
+        System.out.println( "Hello World!" );
+    }
+}
